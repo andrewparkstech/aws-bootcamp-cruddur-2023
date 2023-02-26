cf9e0a99fcfe1f2d42e582e9b63aef897bd9fd24
Andrew Parks <aparks@gmail.com>
2023-02-26 00:20:17 +0000

    fixed spelling

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta, timezone
 
 # Honeycomb
 from opentelemetry import trace
-tracer = trace.get_tracer("home.activites")
+tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run():
