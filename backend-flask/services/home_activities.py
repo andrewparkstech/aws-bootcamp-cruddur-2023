d315beb172b91a51eb746ac51bb9210b5c47a07e
Andrew Parks <aparks@gmail.com>
2023-02-26 00:41:33 +0000

    added more honeycomb

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -6,9 +6,13 @@ tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run():
+
     # Honeycomb
     with tracer.start_as_current_span("home-activities-mock-data"):
+      span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
+      span.set_attribute("app.now", now.isoformat())
+      
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Andrew Brown',
@@ -48,4 +52,5 @@ class HomeActivities:
         'replies': []
       }
       ]
+    span.set_attribute("app.result_length", len(results))
     return results
