cbc37d00722c24d595d81f29690a663579e1ac7e
Andrew Parks <aparks@gmail.com>
2023-07-25 21:52:42 +0000

    fixed seed data cognito IDs - 500 error fixed

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
+  ('Andrew Brown','redacted+email@gmail.com' , 'andrewbrown' ,'ab84e029-8098-41fa-83ee-5bf05b8c171f'),
+  ('Andrew Parks','redacted+email@gmail.com' , 'aparks' ,'96e9596a-fa80-4e15-90f3-0191e76a51b0'),
   ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
